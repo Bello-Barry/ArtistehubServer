307ac05bdcb7fc37b7d45d5215564f46974d95e8
Bello-Barry <79174154+Bello-Barry@users.noreply.github.com>
2024-07-03 03:30:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const io = new Server(server, {
   },
 });
 
-app.get('/post/PostView', (req, res) => {
-  res.send('Socket.IO server is running');
-});
 
 io.on('connection', (socket) => {
   console.log('a user connected');
